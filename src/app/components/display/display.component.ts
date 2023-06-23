d6a7155c87dc7f6a342a4a52920f768ab5fe9d30
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-06-23 00:28:41 -0300

    Added half logic to display

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemsService } from 'src/app/services/items.service';
 
 @Component({
   selector: 'app-display',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayComponent implements OnInit {
 
-  constructor() { }
+  total: number = 0;
+
+  constructor(private itemService: ItemsService) { 
+    this.getTotal();
+  }
 
   ngOnInit(): void {
+  }
+
+  getTotal(){
+    this.total = 0;
+    let items = this.itemService.getItems();
+    items.forEach( item => this.total += item.price)
   }
 
 }
